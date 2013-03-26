2e82e32cfdab756bfb690fb998b925af985c6e19
Karan Goel <karan@goel.im>
2013-03-26 13:39:23 -0700

    Bare bone structure!

---
+++ src/utils/Langs.java
@@ -0,0 +1,30 @@
+package utils;
+
+public class Langs {
+
+	public Langs() {}
+
+	private String[] langs = {
+			"All Languages", 
+			"English", 
+			"Mandarin", 
+			"Spanish", 
+			"Hindi", 
+			"Arabic", 
+			"Bengali", 
+			"Potugese", 
+			"Russian", 
+			"Japanese", 
+			"Korean", 
+			"German", 
+			"French", 
+			"Italian", 
+			"Thai", 
+			"Punjabi"
+	};
+	
+	public String[] getLangs() {
+		return langs;
+	}
+
+}
