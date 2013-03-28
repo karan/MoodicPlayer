66ad9bff21d7db8cb99c001c2ba05d2dc3135cca
Karan Goel <karan@goel.im>
2013-03-28 10:20:47 -0700

    I'm proud of my first multi-threaded app
    So it took a lot of time to understand how progress bar worked. But
    finally, my first multi-threaded app. Now, need to implement an audio
    player, that is if last.fm provides streaming! :O

--- src/im/goel/MoodicPlayer/MoodicPlayer.java
+++ src/im/goel/MoodicPlayer/MoodicPlayer.java
@@ -1,7 +1,6 @@
 package im.goel.MoodicPlayer;
 
 /**
- * TODO: Consider using LinkedList instead of ArrayList
  * @see JaCo: http://jacomp3player.sourceforge.net/guide/javadocs/jaco/mp3/player/MP3Player.html
  * @see JGroove: http://jgroove.googlecode.com/svn/trunk/javadoc/index.html
  */
@@ -12,6 +11,8 @@ import java.awt.GridLayout;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -22,12 +23,14 @@ import java.nio.channels.ReadableByteChannel;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
+import javax.swing.SwingWorker;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -39,7 +42,7 @@ import org.jdom2.input.SAXBuilder;
 import utils.Moods;
 
 
-public class MoodicPlayer implements ActionListener {
+public class MoodicPlayer implements ActionListener, PropertyChangeListener {
 
 	/**
 	 * @param args
@@ -50,11 +53,12 @@ public class MoodicPlayer implements ActionListener {
 		new MoodicPlayer();
 	}
 
-	private List<TrackInfo> allTracks = new ArrayList<TrackInfo>();
-	Moods moodObj = new Moods();
+	private List<TrackInfo> allTracks;
+	private List<String> mbids;
+	private Moods moodObj = new Moods();
 	private JFrame frame;
 	private JPanel north, south;
-	private JProgressBar progress; // TODO: Worry about this later!
+	private JProgressBar progress;
 	private JComboBox<String> moodSelector;
 	private JButton go;
 
@@ -86,32 +90,95 @@ public class MoodicPlayer implements ActionListener {
 
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == go) {
+			progress.setIndeterminate(true);
 			go.setEnabled(false);
-			long start = System.currentTimeMillis();
+			Task task = new Task();
+			task.addPropertyChangeListener(this);
+			task.execute();
+		}
+	}
+
+	public void propertyChange(PropertyChangeEvent e) {
+		if ("progress" == e.getPropertyName()) {
+			int p = (Integer) e.getNewValue();
+			progress.setIndeterminate(false);
+			System.out.println(p * (100 / mbids.size()) + (100 % mbids.size()));
+			progress.setValue(p * (100 / mbids.size()) + (100 % mbids.size()));
+		}
+	}
+
+	//********************** BUILD GUI **********************//
+	private void initializeFrame() {
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(new Dimension(300, 110));
+		frame.setLocation(new Point(400, 300));
+		frame.setTitle("Moodic Player");
+		frame.setResizable(false);
+		frame.setLayout(new BorderLayout());
+	}
+
+	private void initializeNorth() {
+		north = new JPanel(new GridLayout(1, 3));
+		moodSelector = new JComboBox<String>(moodObj.getMoods());
+		go = new JButton("Play");
+		go.addActionListener(this);
+		initializeProgress();
+		north.add(moodSelector);
+		north.add(go);
+		north.add(progress);
+		north.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+	}
+
+	private void initializeProgress() {
+		progress = new JProgressBar(0, 100);
+		progress.setValue(0);
+	}
+
+	private void initializeSouth() {
+		south = new JPanel(new BorderLayout(1, 2));
+		south.add(new JLabel("Created by Karan Goel || http://www.goel.im"));
+		south.setBorder(BorderFactory.createEmptyBorder(15, 10, 5, 10));
+	}
+	//********************** BUILD GUI **********************//
+
+
+	public class Task extends SwingWorker<Void, Object> {
+
+		@Override
+		protected Void doInBackground() {
 			String mood = moodSelector.getSelectedItem().toString().toLowerCase();
 			List<String> subMoods = moodObj.getSubMoods(mood);
+			String key = "3a5d7dcb68d7fb252365acc1ed4eb32d";
+			mbids = getAllMbids(subMoods, key);
+			int currentProgress = 0;
+			int maxProgress = mbids.size();
+			//Initialize progress property.
+			setProgress(1);
+			allTracks = new ArrayList<TrackInfo>();
+			while (currentProgress < maxProgress) {
+				// Make progress.
+				// Run this once for ever mbid
+				getAllTracksForMbid(mbids.get(currentProgress), key);
+				currentProgress = allTracks.size();
+				setProgress(Math.min(currentProgress, maxProgress));
+			}
+			return null;
+		}
 
-			String key = "6ea9780361e0c7342a8a08a1ad78dfec";
+		public void done() {
+			go.setEnabled(true);
+			System.out.println(allTracks.toString());
+		}
 
-			List<String> allMbids = new ArrayList<String>();
-
+		private List<String> getAllMbids(List<String> subMoods, String key) {
 			// START building list of MBID
+			List<String> allMbids = new ArrayList<String>();
 			for (String tag : subMoods) {
-				SAXBuilder builder = new SAXBuilder();
 				String urlToParse = "http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&" +
-							"tag=" + tag + "&api_key=" + key + "&limit=5";
+						"tag=" + tag + "&api_key=" + key + "&limit=5";
 				try {
-					URL website = new URL(urlToParse);
-					ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-					FileOutputStream fos = new FileOutputStream("request.xml");
-					fos.getChannel().transferFrom(rbc, 0, 1 << 24);
-
-					File xmlFile = new File("request.xml");
-
-					Document document = (Document) builder.build(xmlFile);
-					Element rootNode = document.getRootElement();
-					List<Element> list = rootNode.getChildren("toptracks");
-
+					List<Element> list = getList(urlToParse, "tag.gettoptracks");
 					for (int i = 0; i < list.size(); i++) {
 						Element node = (Element) list.get(i);
 						List<Element> subList = node.getChildren("track");
@@ -132,78 +199,56 @@ public class MoodicPlayer implements ActionListener {
 				}
 			}
 			// END building list of MBID
+			return allMbids;
+		}
 
+		private void getAllTracksForMbid(String mbid, String key) {
 			// START building list of Info
-			for (String mbid : allMbids) {
-				SAXBuilder builder = new SAXBuilder();
-				String urlToParse = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&" + 
-								"api_key=" + key + "&mbid=" + mbid;
-				try {
-					URL website = new URL(urlToParse);
-					ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-					FileOutputStream fos = new FileOutputStream("request.xml");
-					fos.getChannel().transferFrom(rbc, 0, 1 << 24);
-	
-					File xmlFile = new File("request.xml");
-	
-					Document document = (Document) builder.build(xmlFile);
-					Element rootNode = document.getRootElement();
-					List<Element> trackList = rootNode.getChildren("track");
-					for (int i = 0; i < trackList.size(); i++) {
-						Element node = (Element) trackList.get(i);
-						String name = node.getChildText("name");
-						int playCount = Integer.parseInt(node.getChildText("playcount"));
-						int listeners = Integer.parseInt(node.getChildText("listeners"));
-						List<Element> artistList = node.getChildren("artist");
-						for (int j = 0; j < artistList.size(); j++) {
-							Element artistElement = (Element) artistList.get(j);
-							allTracks.add(new TrackInfo(name, artistElement.getChildText("name"), 
-									mbid, playCount, listeners));
-						}
+			String urlToParse = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&" + 
+					"api_key=" + key + "&mbid=" + mbid;
+			try {
+				List<Element> trackList = getList(urlToParse, "track.getInfo");
+				for (int i = 0; i < trackList.size(); i++) {
+					Element node = (Element) trackList.get(i);
+					String name = node.getChildText("name");
+					int playCount = Integer.parseInt(node.getChildText("playcount"));
+					int listeners = Integer.parseInt(node.getChildText("listeners"));
+					List<Element> artistList = node.getChildren("artist");
+					for (int j = 0; j < artistList.size(); j++) {
+						Element artistElement = (Element) artistList.get(j);
+						allTracks.add(new TrackInfo(name, artistElement.getChildText("name"), 
+								mbid, playCount, listeners));
 					}
-				} catch (MalformedURLException e1) {
-					e1.printStackTrace();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				} catch (JDOMException e1) {
-					e1.printStackTrace();
 				}
+			} catch (MalformedURLException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			} catch (JDOMException e1) {
+				e1.printStackTrace();
 			}
 			// END building list of Info
-
-			long end = System.currentTimeMillis();
-			System.out.println(end - start + " ms");
-			
-			System.out.println(allTracks.toString());
 		}
-	}	
 
-	//********************** BUILD GUI **********************//
-	private void initializeFrame() {
-		frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(new Dimension(300, 80));
-		frame.setLocation(new Point(400, 300));
-		frame.setTitle("Moodic Player");
-		frame.setResizable(false);
-		frame.setLayout(new BorderLayout());
-	}
+		private List<Element> getList(String urlToParse, String method) throws IOException, JDOMException {
+			SAXBuilder builder = new SAXBuilder();
+			URL website = new URL(urlToParse);
+			ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+			FileOutputStream fos = new FileOutputStream("request.xml");
+			fos.getChannel().transferFrom(rbc, 0, 1 << 24);
+			
+			File xmlFile = new File("request.xml");
+			
+			Document document = (Document) builder.build(xmlFile);
+			Element rootNode = document.getRootElement();
+			
+			if (method.equals("tag.gettoptracks")) {
+				return rootNode.getChildren("toptracks");
+			} else {
+				return rootNode.getChildren("track");
+			}
+		}
 
-	private void initializeNorth() {
-		north = new JPanel(new GridLayout(1, 3));
-		moodSelector = new JComboBox<String>(moodObj.getMoods());
-		go = new JButton("< Play >");
-		go.addActionListener(this);
-		progress = new JProgressBar(1, 100);
-		north.add(progress);
-		north.add(moodSelector);
-		north.add(go);
 	}
-
-	private void initializeSouth() {
-		south = new JPanel(new BorderLayout(1, 2));
-		south.add(new JLabel("Created by Karan Goel || http://www.goel.im"));
-	}
-	//********************** BUILD GUI **********************//
 
 }
